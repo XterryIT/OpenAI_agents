cd0e29399c645547f0beff702054e074cbed0445
XterryIT <vptashka2@gmail.com>
2025-10-09 19:57:49 +0000

    implement a full fuction search agent

--- search_agent/agent.py
+++ search_agent/agent.py
@@ -1,6 +1,16 @@
 from google.adk.agents import Agent
 import dotenv
+from google.adk.tools import google_search
 
 dotenv.load_dotenv() # Load environment variables from .env file
 
 MODEL = "gemini-2.5-flash"
+
+
+root_agent = Agent(
+    name="search_agent",
+    model=MODEL,
+    instruction="You have access to the search trough the internet.",
+    tools=[google_search],
+
+)
